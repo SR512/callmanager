5e7ec0560db00556f380532ee0decde55c4e14cd
in-sanjay-makwana <sanjay.makwana@tiez.nl>
2022-09-13 01:29:38 +0530

    download button added

--- www/resources/views/admin/layouts/topbar.blade.php
+++ www/resources/views/admin/layouts/topbar.blade.php
@@ -324,9 +324,9 @@
 {{--            </div>--}}
 
             <div class="dropdown d-none d-lg-inline-block ms-1">
-                <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
-                    <i class="bx bx-fullscreen"></i>
-                </button>
+                <a type="button" href="{{asset('apk/callmanager-v1.0.apk')}}" target="_blank" class="btn header-item noti-icon waves-effect">
+                    <i class="bx bx-download"></i>&nbsp;Download apk
+                </a>
             </div>
 
 {{--            <div class="dropdown d-inline-block">--}}
